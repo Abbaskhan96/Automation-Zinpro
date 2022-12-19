0792d021da982588d8aee1d063aa1dd0b1c03cbc
QA Abbas <khanabbas000@gmail.com>
2022-12-19 17:36:12 +0500

    Heat-Abatement Excell importing completed...

--- All_assessors_xpath/xpath_heat_abate.py
+++ All_assessors_xpath/xpath_heat_abate.py
@@ -118,15 +118,16 @@ class Assessors_HeatAbatement_xpath():
 #my_final_dict = {
 #    WA_yes
 #    }
-        def __new__(self, driver):
+        def __init__(self, driver):
             self.driver= driver
-            file= openpyxl.load_workbook("C:/Users/CDC.CDC-PC/source/repos/Automation-Zinpro/Excell_Files/Locators.xlsx")
-           # self.file = openpyxl.load_workbook("C:/Users/Muhammad Abbas Khan/Source/Repos/Automation-Zinpro/Excell_files/Locators.xlsx")
-            sheet = file["HeatAbatement"] 
-            _global = Assessors_HeatAbatement_xpath
-            self.driver.find_element(By.XPATH, _global.accessible_water_pen).send_keys(sheet.cell(9,7).value)
+           # file= openpyxl.load_workbook("C:/Users/CDC.CDC-PC/source/repos/Automation-Zinpro/Excell_Files/Locators.xlsx")
+            self.file = openpyxl.load_workbook("C:/Users/Muhammad Abbas Khan/Source/Repos/Automation-Zinpro/Excell_files/Locators.xlsx")
+            self.sheet = self.file["HeatAbatement"]
             
-            data_click = { 'yes' : ( _global.WA_yes, _global.Warea_yes,
+            _global = Assessors_HeatAbatement_xpath
+         #   self.driver.find_element(By.XPATH, _global.accessible_water_pen).send_keys(sheet.cell(9,7).value)
+            
+            self.data_click = { 'yes' : ( _global.WA_yes, _global.Warea_yes,
                                    _global.FL_FA_yes, _global.HVLS_FA_yes, 
                                    _global.FL_RA_yes ,_global.HVLS_RA_yes, 
                                    _global.Soak_FA_yes, 
@@ -143,28 +144,37 @@ class Assessors_HeatAbatement_xpath():
                                     _global.Shade_RA_no, _global.Shade_unit_no)}
             
             
-            data_keys = {_global.FL_FA_yes : (_global.Fan_FA_temp, _global.Fan_FA_space),
+            self.data_keys = {_global.FL_FA_yes : (_global.Fan_FA_temp, _global.Fan_FA_space),
                          _global.FL_RA_yes : (_global.Fan_RA_temp, _global.Fan_RA_space),
                          _global.Soak_RA_yes : (_global.Soak_temp, _global.Soak_freq)}
 
-            data_select = {
-                           _global.FL_FA_yes : _global.Fan_RA_diamter,
+            self.data_select = {
+                           _global.FL_FA_yes : _global.Fan_FA_diamter,
                            _global.FL_RA_yes : _global.Fan_RA_diamter
                             }
             
             """
-            data_select = {_global.Fan_FA_diamter : 
+            self.data_select = {_global.Fan_FA_diamter : 
                         }
            """ 
+  
+        def data_sheet(self,start_rows, last_rows,  column):    
+            
             path_yes = None
             path_yes=[]
-            i,j = 10,0
+            i,j = start_rows,0
             
+            _global = Assessors_HeatAbatement_xpath
+            self.driver.find_element(By.XPATH,_global.accessible_water_pen).clear()
+            self.driver.find_element(By.XPATH,_global.accessible_water_pen).send_keys(self.sheet.cell(start_rows - 1,column).value)
             
-            while (i!=22 and j!=12):
+            while (i!=last_rows+1 and j!=12):
                 
-                """ store the data from conditions of excell sheet either the excell dropdown (yes/no) and store in path_yes"""
-                _get_click_data = data_click[sheet.cell(i,7).value][j]
+                
+                
+
+                """ store the data from conditions of excell self.sheet either the excell dropdown (yes/no) and store in path_yes"""
+                _get_click_data = self.data_click[self.sheet.cell(i,column).value][j]
 
                 print(f"{i},{j}","\n",_get_click_data,"\n","\n")
                 path_yes.append(_get_click_data)
@@ -179,21 +189,29 @@ class Assessors_HeatAbatement_xpath():
                     self.driver.find_element(By.XPATH, _get_click_data).click()
                     
                     """ get the list of Temperature and space"""
-                    temp_space = data_keys[_get_click_data]
+                    temp_space = self.data_keys[_get_click_data]
                    # print("send arrays")
                    # print(temp_space)
                     for k,l in zip(temp_space, range(i+11,i+13)):
                         
                       #  print("values")
-                      #  print(sheet.cell(l,7).value)
+                      #  print(self.sheet.cell(l,7).value)
                       # send_keys from Excell Cell
                      # sending keys from excell cell to Temp,Space text site field 
                         
-                        self.driver.find_element(By.XPATH, k).send_keys(sheet.cell(l,7).value)
+                        self.driver.find_element(By.XPATH, k).send_keys(self.sheet.cell(l,column).value)
                     if _global.Soak_RA_yes in path_yes:
-                        self.driver.find_element(By.XPATH, _global.Soak_time).send_keys(sheet.cell(30,7).value) 
+                        self.driver.find_element(By.XPATH, _global.Soak_time).send_keys(self.sheet.cell(30,column).value) 
                         self.driver.find_element(By.XPATH, _global.SoakFreq_VT_yes).click() 
                 
+                    """
+                Checking that the Fans in Feed Area/Resting areas are available
+                """
+                    if ((_get_click_data == _global.FL_FA_yes) or
+                    (_get_click_data == _global.FL_RA_yes)):
+                        print(f"{i+20},{column}")
+                        self.selecting_dropdown(self.data_select[_get_click_data], (self.sheet.cell(i+20,column).value), self.driver)
+                        
               
                 """ for skipping the dependent field if their parents answer is no"""
                 if ((_get_click_data == _global.FL_FA_no) or 
@@ -202,16 +220,12 @@ class Assessors_HeatAbatement_xpath():
                     (_get_click_data == _global.Shade_RA_no)):
                     i+=1 
                     j+=1
-
+  
                 i=i+1
                 j=j+1
 
-                """
-                Checking that the Fans in Feed Area/Resting areas are available
-                """
-                if ((_get_click_data == _global.FL_FA_yes) or
-                    (_get_click_data == _global.FL_RA_yes)):
-                    self.selecting_dropdown(data_select[_get_click_data], sheet.cell(i+21,7).value, self.driver)
+        
+          
                
             print("Final list <Return>","\n")
             print(i for i in path_yes)
@@ -222,12 +236,15 @@ class Assessors_HeatAbatement_xpath():
 
 
 
-        def selecting_dropdown(xpath, drop_sheet_value, driver):
+        def selecting_dropdown(self,xpath, drop_sheet_value, driver):
             #for i,j in zip(loc.select_fields,index):
+            print("Excell Value","\n")
+            print(drop_sheet_value)
             drop_field = Select(driver.find_element(By.XPATH,xpath))
             time.sleep(1)
             drop_field.select_by_visible_text(str(drop_sheet_value))
             time.sleep(2)
+            return
 #            for i,j in zip(range(10,16), range(6)):
 #                #print(sheet.cell(i,7).value, data[sheet.cell(i,7).value][j])
 #                #path_yes = data[sheet.cell(i,7).value][j]
--- Locators_xpath/Assessors_access/Heat_Abatement.py
+++ Locators_xpath/Assessors_access/Heat_Abatement.py
@@ -36,60 +36,45 @@ class heat_assessor():
     def closed_HeatAbatement(self):
         loc = Assessors_HeatAbatement_xpath(self.driver)
 #        func = lambda : [i for i in loc]
-        (self.driver.find_element(By.XPATH, i).click() for i in loc)
-        for i in loc:
+        data = loc.data_sheet(10,21,7)
+        for i in data:
+           # print(i,"\n")
             self.driver.find_element(By.XPATH, i).click()
-#        self.driver.find_element(By.XPATH, loc).click()
-#        [i for i in loc.path_yes.keys()]
-        #input data into fields....
-
+        time.sleep(2)
+    
         
-#        self.driver.find_element(By.XPATH, loc.Fan_FA_temp).send_keys("20")
-#        self.driver.find_element(By.XPATH, loc.Fan_FA_space).send_keys("12")
-#        self.driver.find_element(By.XPATH, loc.Fan_RA_temp).send_keys("21")
-#        self.driver.find_element(By.XPATH, loc.Fan_RA_space).send_keys("5")
-#        self.driver.find_element(By.XPATH, loc.Soak_temp).send_keys("21")
-#        self.driver.find_element(By.XPATH, loc.Soak_time).send_keys("15")
-#        self.driver.find_element(By.XPATH, loc.Soak_freq).send_keys("60")
-#        time.sleep(2)
-
-        # selecting Dropdowns fields
-        index = [7,3]
-        self.selecting_dropdown(index)
-
 
     def faroff_HeatAbatement(self):
-        self.driver.find_element(By.XPATH, loc.accessible_water_pen).send_keys("600")
-        func = lambda : [i for i in loc.path_no]
-        [self.driver.find_element(By.XPATH, i).click() for i in func()]
+ 
+        loc = Assessors_HeatAbatement_xpath(self.driver)
+#        func = lambda : [i for i in loc]
+        data = loc.data_sheet(10,21,9)
+        for i in data:
+          #  print(i,"\n")
+            self.driver.find_element(By.XPATH, i).click()
         time.sleep(2)
 
 
     
     def fresh_HeatAbatement(self):
-        self.driver.find_element(By.XPATH, loc.accessible_water_pen).send_keys("1100")
-        func = lambda : [i for i in loc.path_N_A]
-        [self.driver.find_element(By.XPATH, i).click() for i in func()]
+        
+
+        loc = Assessors_HeatAbatement_xpath(self.driver)
+#        func = lambda : [i for i in loc]
+        data = loc.data_sheet(10,21,11)
+        for i in data:
+          #  print(i,"\n")
+            self.driver.find_element(By.XPATH, i).click()
         time.sleep(2)
 
 
     def lactating_HeatAbatement(self):
-        func = lambda : [i for i in loc.path_yes_no]
-        [self.driver.find_element(By.XPATH, i).click() for i in func()]
+        
+
+        loc = Assessors_HeatAbatement_xpath(self.driver)
+#        func = lambda : [i for i in loc]
+        data = loc.data_sheet(10,21,13)
+        for i in data:
+          #  print(i,"\n")
+            self.driver.find_element(By.XPATH, i).click()
         time.sleep(2)
-
-
-        #inserting Data into Fields
-        self.driver.find_element(By.XPATH, loc.accessible_water_pen).send_keys("3000")
-        self.driver.find_element(By.XPATH, loc.Fan_FA_temp).send_keys("32")
-        self.driver.find_element(By.XPATH, loc.Fan_FA_space).send_keys("29")
-        self.driver.find_element(By.XPATH, loc.Fan_RA_temp).send_keys("38")
-        self.driver.find_element(By.XPATH, loc.Fan_RA_space).send_keys("41")
-        self.driver.find_element(By.XPATH, loc.Soak_temp).send_keys("26")
-        self.driver.find_element(By.XPATH, loc.Soak_time).send_keys("30")
-        self.driver.find_element(By.XPATH, loc.Soak_freq).send_keys("120")
-        time.sleep(2)
-
-        # selecting Dropdowns fields
-        index = [9,1]
-        self.selecting_dropdown(index)
